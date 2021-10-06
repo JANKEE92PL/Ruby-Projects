0eb987562c6b2138bc9a6d25feb9453c6b129638
JANKEE92PL <jankee112@freenet.de>
2021-10-06 14:48:27 +0200

    bubble sort exercise done

---
+++ bubble sort/bubble_sort.rb
@@ -0,0 +1,54 @@
+def bubble_sort(array)
+
+  unsorted = true
+
+
+
+  while unsorted
+
+
+
+    i = 0
+
+
+
+    unsorted = false
+
+
+
+    while i < (array.length - 1)
+
+
+
+      if array[i] > array[i + 1]
+
+
+
+        array[i], array[i + 1] = array[i + 1], array[i]
+
+
+
+        unsorted = true
+
+
+
+      end
+
+
+
+      i += 1
+
+
+
+    end
+
+
+
+  end
+
+
+
+  print array
+
+end
+
